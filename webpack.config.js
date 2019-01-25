efba8b47a589d83bb142f4255402343664433b3e
Masaya Takeda <844196@gmail.com>
2019-01-25 20:32:07 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,8 @@
-const nodeExternals = require('webpack-node-externals');
-
 module.exports = {
   target: 'node',
   externals: [
-    nodeExternals({
-      whitelist: [
-        'fp-ts/lib/Option',
-        'fp-ts/lib/Either',
-        'fp-ts/lib/TaskEither',
-        'jsdom',
-        'pn/fs',
-      ],
-    }),
+    'bufferutil',
+    'utf-8-validate',
     'canvas',
   ],
 };
