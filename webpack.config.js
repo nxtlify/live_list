83746719ef9fa78685599355ee391a2150169b78
Masaya Takeda <844196@gmail.com>
2019-01-25 20:21:14 +0900

    fix node externals option

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   target: 'node',
   externals: [
-    nodeExternals(),
+    nodeExternals({
+      whitelist: [
+        'fp-ts/lib/Option',
+        'fp-ts/lib/Either',
+        'fp-ts/lib/TaskEither',
+        'jsdom',
+      ],
+    }),
+    'canvas',
   ],
 };
